ecb94859621139c4894ee97fc1b8d94397993035
classyrazy <classyrazy014@gmail.com>
2023-06-01 06:58:56 +0100

    Developing

---
+++ gsap-locomotive-web-unlocked-1/src/components/NavBar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style.css"
+function Navbar() {
+  return (
+    <div className="navbar">
+        <div className="">Menu</div>
+        <div className="">Flirty Flowers</div>
+        <div className="">Cart</div>
+    </div>
+  )
+}
+
+export default Navbar
